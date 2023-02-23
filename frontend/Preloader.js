57ba87d9c20c56f9dacd4302482db23c62e27f97
ibtisam midlet <storebitid@gmail.com>
2023-02-23 11:16:34 -0800

    *

---
+++ frontend/Preloader.js
@@ -0,0 +1,8 @@
+/*
+// Preloader
+ 
+$(window).bind(&quot;load&quot;, function () {
+    jQuery(&quot;#loader&quot;).fadeOut(&quot;slow&quot;);
+    jQuery(&quot;#preloader&quot;).delay(0).fadeOut();
+});
+*/
