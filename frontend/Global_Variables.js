3f64c87c1ef76cc6fcce545401fa6835a12e199a
ibtisam midlet <storebitid@gmail.com>
2023-05-21 15:51:53 +0100

    c

--- frontend/Global_Variables.js
+++
@@ -1,6 +0,0 @@
-// Global variables with content. "Available for Edit"
-var monthFormat = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    noThumbnail = "https://4.bp.blogspot.com/-O3EpVMWcoKw/WxY6-6I4--I/AAAAAAAAB2s/KzC0FqUQtkMdw7VzT6oOR_8vbZO6EJc-ACK4BGAYYCw/s1600/nth.png",
-    postPerPage = 8,
-    commentsSystem = "blogger",
-    disqusShortname = "soratemplates";
